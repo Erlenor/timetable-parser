1e8260e90fa2c5bfd36b633cd31bb4dcf772cbc2
filiphovorka <filip.hovorka@lyceum.sk>
2026-04-22 13:04:19 +0200

    remove config.js from tracking

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    GOOGLE_CLIENT_ID: '457295572907-n91eqcdjhl1ev9aj7k94qliccaln4a2c.apps.googleusercontent.com',
-    GEMINI_API_KEY: 'AIzaSyDku_ucYfrysI4rIHKG9tCMZHO9FPFltkc'
-};
